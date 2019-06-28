52bcb08a6eefd001c3701f01ba15e849849f4c6a
hameru <47452818+hameru@users.noreply.github.com>
2019-06-28 04:58:30 -0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -21,34 +21,34 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith('is shb out'):
-        randomlist = ["no","nope","not yet","This Long https://www.timeanddate.com/countdown/launch?iso=20190628T05&p0=179&msg=SHADOWBRINGERS&ud=1&font=cursive&csz=1"]
+        randomlist = ["YES! FUCK OFF!"]
         await client.send_message(message.channel,(random.choice(randomlist)))
     if message.content.startswith('is shadowbringers out'):
-        randomlist = ["no","nope","not yet","This Long https://www.timeanddate.com/countdown/launch?iso=20190628T05&p0=179&msg=SHADOWBRINGERS&ud=1&font=cursive&csz=1"]
+        randomlist = ["YES!"]
         await client.send_message(message.channel,(random.choice(randomlist)))
 
     if message.content.startswith('is Shadowbringers out'):
-        randomlist = ["no","nope","not yet","This Long https://www.timeanddate.com/countdown/launch?iso=20190628T05&p0=179&msg=SHADOWBRINGERS&ud=1&font=cursive&csz=1"]
+        randomlist = ["YES!"]
         await client.send_message(message.channel,(random.choice(randomlist)))
     if message.content.startswith('is Shb out'):
-        randomlist = ["no","nope","not yet","This Long https://www.timeanddate.com/countdown/launch?iso=20190628T05&p0=179&msg=SHADOWBRINGERS&ud=1&font=cursive&csz=1"]
+        randomlist = ["YES!"]
         await client.send_message(message.channel,(random.choice(randomlist)))
     if message.content == 'how long until shb':
-        await client.send_message(message.channel,'https://www.timeanddate.com/countdown/launch?iso=20190628T05&p0=179&msg=SHADOWBRINGERS&ud=1&font=cursive&csz=1')
+        await client.send_message(message.channel,'GO PLAY!')
     if message.content.startswith('Is shb out'):
-        randomlist = ["no","nope","not yet","This Long https://www.timeanddate.com/countdown/launch?iso=20190628T05&p0=179&msg=SHADOWBRINGERS&ud=1&font=cursive&csz=1"]
+        randomlist = ["YES!"]
         await client.send_message(message.channel,(random.choice(randomlist)))
     if message.content.startswith('Is shadowbringers out'):
-        randomlist = ["no","nope","not yet","This Long https://www.timeanddate.com/countdown/launch?iso=20190628T05&p0=179&msg=SHADOWBRINGERS&ud=1&font=cursive&csz=1"]
+        randomlist = ["YES!"]
         await client.send_message(message.channel,(random.choice(randomlist)))
 
     if message.content.startswith('Is Shadowbringers out'):
-        randomlist = ["no","nope","not yet","This Long https://www.timeanddate.com/countdown/launch?iso=20190628T05&p0=179&msg=SHADOWBRINGERS&ud=1&font=cursive&csz=1"]
+        randomlist = ["YES!",]
         await client.send_message(message.channel,(random.choice(randomlist)))
     if message.content.startswith('Is Shb out'):
-        randomlist = ["no","nope","not yet","This Long https://www.timeanddate.com/countdown/launch?iso=20190628T05&p0=179&msg=SHADOWBRINGERS&ud=1&font=cursive&csz=1"]
+        randomlist = ["YES!"]
         await client.send_message(message.channel,(random.choice(randomlist)))
     if message.content == 'How long until shb':
-        await client.send_message(message.channel,'https://www.timeanddate.com/countdown/launch?iso=20190628T05&p0=179&msg=SHADOWBRINGERS&ud=1&font=cursive&csz=1')
+        await client.send_message(message.channel,'GO PLAY!')
         
 client.run('NTQzNDExNzk2MDU3MzkxMTM0.Dz-n4A.2j1KT-Aaz4Fw7iTfWXLKHJplQWQ')
